63bce7feffe227771e587cab8ad29e61eb30dbba
Kilian <kilianpadilla@hotmail.com>
2023-01-23 14:54:25 +0100

    Commit inicial

---
+++ Practica-V-Herramientas-colaborativas/Practica-V-Herramientas-colaborativas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_V_Herramientas_colaborativas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
